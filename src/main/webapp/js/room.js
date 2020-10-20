d5f7ed0a93ab65602c4ca2248a1b5bb9322b88b8
Thai Nguyen <ndt.rachkien@gmail.com>
2020-10-20 17:18:17 -0500

    CRUD Room Login/Logout

--- src/main/webapp/js/room.js
+++ src/main/webapp/js/room.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("submitroom").click(function (event){
+    $("#submitroom").click(function (event){
         var roomNo = $("#roomno").val();
         var roomType = $("#roomtype").val();
         var roomPrice = $("#roomprice").val();
@@ -13,11 +13,27 @@ $(document).ready(function(){
         }).done(function(responseJson){
             var str = "<table>";
             $.each(responseJson, function(key, value){
+                if(key == 0){
+                    str += "<tr>";
+                    str += "<th>" + "RoomNo" + "</th>"
+                    str += "<th>" + "Type" + "</th>"
+                    str += "<th>" + "Price" + "</th>"
+                    str += "<th>" + "Max Guest" + "</th>"
+                    str += "<th>" + "Max Guest" + "</th>"
+                    str += "<th>" + "Action" + "</th>"
+                    str += "</tr>";
+                }
+
                 str += "<tr>";
-                    str += "<td>" + value + "</td>";
+                str += "<td>" + value.roomNo + "</td>";
+                str += "<td>" + value.type + "</td>";
+                str += "<td>" + value.price + "</td>";
+                str += "<td>" + value.guestNum + "</td>";
+                str += "<td>" + "<a href='url'>" + "Delete" + "</a>" + "<a href='url'>" + "Delete" + "</a>" + "</td>";
                 str += "</tr>";
             });
-            $("listroom").html(str);
+            // alert(str);
+            $("#listroom").html(str);
         });
     });
 })
