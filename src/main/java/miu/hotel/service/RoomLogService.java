b4d0e5485b4a56a02c5cd6a4ce617a7f32b5597f
thainguyen <thai.nguyen.1093>
2020-10-21 18:18:25 -0500

    Thai- add room log service to get latest by room no

--- src/main/java/miu/hotel/service/RoomLogService.java
+++ src/main/java/miu/hotel/service/RoomLogService.java
@@ -1,13 +1,19 @@
 package miu.hotel.service;
 
 import miu.hotel.database.RoomLogs;
-import miu.hotel.database.Rooms;
-import miu.hotel.model.Room;
 import miu.hotel.model.RoomLog;
 
 public class RoomLogService {
   public static boolean add(RoomLog roomLog) {
     RoomLogs.roomList.add(roomLog);
     return true;
+  }
+
+  public static RoomLog getLatestRoomLogByRoomNo(String roomNo) {
+    return RoomLogs.roomList
+        .stream()
+        .filter(roomLog -> roomLog.getRoomNo().equals(roomNo))
+        .reduce((first, second) -> second)
+        .orElse(null);
   }
 }
